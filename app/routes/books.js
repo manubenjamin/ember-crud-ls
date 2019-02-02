4aa422ed003a1c5d80646d345b26e6911361794f
Manu Benjamin <manu.b@mstack.com>
2019-02-02 11:21:05 +0530

    Added local storage adapter and serializer

--- app/routes/books.js
+++ app/routes/books.js
@@ -2,18 +2,20 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	model() {
-		return this.store.peekAll('book')
+		return this.store.findAll('book')
 	},
 
 	actions: {
 		createNewBook(bookName) {
-			this.store.createRecord('book', {
+			let book = this.store.createRecord('book', {
 				name: bookName,
 				author: 'Chetan Bhagat',
 				coverImage: 'imageurl',
 				price: 400,
 				ratings: 4.8
 			});
+
+			book.save();
 		}
 	}
 });
